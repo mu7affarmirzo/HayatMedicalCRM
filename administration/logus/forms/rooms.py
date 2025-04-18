99ad29620c012ab76ab56e98a5362742860cec6c
Muzaffarmirzo Choriyev <muzaffarmirzo.choriyev@ucell.uz>
2025-04-18 16:28:29 +0500

    🔼Updated: finally fixed form

--- administration/logus/forms/rooms.py
+++ administration/logus/forms/rooms.py
@@ -12,3 +12,9 @@ class RoomForm(forms.ModelForm):
     class Meta:
         model = Room
         fields = ['name', 'room_type', 'price', 'capacity', 'is_active']
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control'}),
+            'room_type': forms.Select(attrs={'class': 'form-control select2'}),
+            'price': forms.NumberInput(attrs={'class': 'form-control'}),
+            'capacity': forms.NumberInput(attrs={'class': 'form-control'}),
+        }

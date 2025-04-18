09e764bf56dd55ebd977808ef6bc346ace76ef1f
Muzaffarmirzo Choriyev <muzaffarmirzo.choriyev@ucell.uz>
2025-04-18 17:01:41 +0500

    ➕Added: index

--- HayatMedicalCRM/urls.py
+++ HayatMedicalCRM/urls.py
@@ -18,8 +18,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 urlpatterns = [
+    path('', TemplateView.as_view(template_name='index.html'), name='home'),
     path('admin/', admin.site.urls),
     path('application/logus/', include('logus.router')),
     path('administration/', include('administration.router')),
